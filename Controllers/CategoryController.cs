5570350f836bdf4c9926008ee9edc768d008180e
IGODX <1dorohov1@gmail.com>
2023-03-25 04:38:28 +0200

    Added few new controllers to distribute load of admin controller.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyShopPet.Data;
+using MyShopPet.Models.ViewModels.CategoryViewModel;
+using MyShopPet.Repositories.Abstraction;
+
+namespace MyShopPet.Controllers
+{
+    [Authorize(Policy = "Admin policy")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryRepository categoryRepository,
+            IMapper mapper)
+        {
+            _mapper = mapper;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult IsCategoryTitleAvailable(string title)
+        {
+            return Json(!_categoryRepository.IsExist(title));
+        }
+
+        public async Task<IActionResult> CreateCategory()
+        {
+            CreateCategoryViewModel vM = new()
+            {
+                ParentCategorySL = new SelectList(await _categoryRepository.GetAll().ToListAsync())
+            };
+            return View(vM);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateCategory(CreateCategoryViewModel vM)
+        {
+            if (!ModelState.IsValid)
+            {
+                SelectList ParentCategorySL = new(await _categoryRepository.GetAll().ToListAsync());
+                vM.ParentCategorySL = ParentCategorySL;
+                return View(vM);
+            }
+            Category category = _mapper.Map<Category>(vM);
+            await _categoryRepository.CreateAsync(category);
+            return RedirectToAction(nameof(Index));
+        }
+        private SelectList GetCategorySelectList(List<Category> categories)
+        {
+            return new SelectList(categories, nameof(Product.Id), nameof(Product.Title));
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,125 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyShopPet.Data;
+using MyShopPet.Models.ViewModels.ProductViewModel;
+using MyShopPet.Repositories.Abstraction;
+using MyShopPet.Services.Abstraction;
+
+namespace MyShopPet.Controllers
+{
+    [Authorize(Policy = "Admin policy")]
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+        public ProductController(IProductRepository productRepository,
+            ICategoryRepository categoryRepository,
+            IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            Product? product = await _productRepository.GetAsync(id);
+            if (product == null)
+                return NotFound();
+            return View(product);
+        }
+        public async Task<IActionResult> Create()
+        {
+            CreateProductViewModel vM = new()
+            {
+                  CategorySL = GetCategorySelectList(await _categoryRepository.GetAll().ToListAsync()),
+             };
+            return View(vM);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateProductViewModel vM, [FromServices] IFileSavingHandler handler)
+        {
+            if (!ModelState.IsValid)
+            {
+                SelectList CategorySL = GetCategorySelectList(await _categoryRepository.GetAll().ToListAsync());
+                vM.CategorySL = CategorySL;
+                return View(vM);
+            }
+            Product product = _mapper.Map<Product>(vM);
+            product.Photos = new();
+            if (vM.Photos != null)
+            {
+                foreach (var photo in vM.Photos)
+                {
+                    string path = await SaveFile(handler, photo);
+                    product.Photos.Add(new Photo { Filename = photo.FileName, PhotoUrl = path });
+                }
+            }
+            await _productRepository.CreateAsync(product);
+            return RedirectToAction(nameof(Index));
+        }
+        private async Task<string> SaveFile([FromServices] IFileSavingHandler handler, IFormFile photo)
+        {
+            string path = await handler.SaveFileAsync(photo);
+            return path;
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _productRepository.GetAsync(id);
+            if (product == null)
+                return NotFound();
+            EditProductViewModel vM = new()
+            {
+                Product = product,
+                CategorySL = GetCategorySelectList(await _categoryRepository.GetAll().ToListAsync()),
+            };
+            return View(vM);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, EditProductViewModel vM)
+        {
+            if (id != vM.Product.Id)
+                return NotFound();
+            if (ModelState.IsValid)
+            {
+                await _productRepository.UpdateAsync(vM.Product);
+                return RedirectToAction("Index", "Admin");
+            }
+            SelectList CategorySL = GetCategorySelectList(await _categoryRepository.GetAll().ToListAsync());
+            vM.CategorySL = CategorySL;
+            return View(vM);
+        }
+      
+        public async Task<IActionResult> Delete(int id)
+        {
+            Product? product = await _productRepository.GetAsync(id);
+            if (product == null)
+                return NotFound();
+            DeleteProductViewModel vM = new()
+            {
+                Product = product
+            };
+            return View(vM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(DeleteProductViewModel vM)
+        {
+            await _productRepository.DeleteAsync(vM.Product);
+            return RedirectToAction("Index");
+        }
+        private SelectList GetCategorySelectList(List<Category> categories)
+        {
+            return new SelectList(categories, nameof(Product.Id), nameof(Product.Title));
+        }
+    }
+}
