34b22beb73c73d93c3a3197b16f0c9ee400a5dd1
IGODX <1dorohov1@gmail.com>
2023-03-25 04:35:31 +0200

    Added folder services, with few services that work with file.

---
+++ Services/UniquePathGenerator.cs
@@ -0,0 +1,20 @@
+﻿using MyShopPet.Services.Abstraction;
+
+namespace MyShopPet.Services
+{
+    public class UniquePathGenerator : IUniquePathGenerator
+    {
+        private readonly IWebHostEnvironment _environment;
+        public UniquePathGenerator(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public (string,string) GeneratePath(IFormFile photo)
+        {
+            string root = $"{Path.GetFileNameWithoutExtension(photo.FileName)}" +
+            $"{Guid.NewGuid()}{Path.GetExtension(photo.FileName)}";
+            root = $"/images/{root}";
+            return (root, _environment.WebRootPath + root);
+        }
+    }
+}
